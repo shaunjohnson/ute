8a734b1825cdaf3beb2222fc6e485804829d816c
Shaun Johnson <shaun@lmxm.net>
2011-06-03 08:53:33 -0400

    UTE-13 Unit test ResourcesUtils.

---
+++ ute/src/test/java/net/lmxm/ute/utils/ResourcesUtilsTest.java
@@ -0,0 +1,57 @@
+/**
+ * Copyright (C) 2011 Shaun Johnson, LMXM LLC
+ * 
+ * This file is part of Universal Task Executor.
+ * 
+ * Universal Task Executor is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by the Free
+ * Software Foundation, either version 3 of the License, or (at your option) any
+ * later version.
+ * 
+ * Universal Task Executor is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
+ * 
+ * You should have received a copy of the GNU General Public License along with
+ * Universal Task Executor. If not, see <http://www.gnu.org/licenses/>.
+ */
+package net.lmxm.ute.utils;
+
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * The Class ResourcesUtilsTest.
+ */
+public class ResourcesUtilsTest {
+
+	/**
+	 * Test get application attributions.
+	 */
+	@Test
+	public void testGetApplicationAttributions() {
+		assertNotNull(ResourcesUtils.getApplicationAttributions());
+		assertTrue(ResourcesUtils.getApplicationAttributions().length() > 0);
+	}
+
+	/**
+	 * Test get application name.
+	 */
+	@Test
+	public void testGetApplicationName() {
+		assertNotNull(ResourcesUtils.getApplicationName());
+		assertTrue(ResourcesUtils.getApplicationName().length() > 0);
+	}
+
+	/**
+	 * Test get application version.
+	 */
+	@Test
+	public void testGetApplicationVersion() {
+		assertNotNull(ResourcesUtils.getApplicationVersion());
+		assertTrue(ResourcesUtils.getApplicationVersion().length() > 0);
+	}
+}
