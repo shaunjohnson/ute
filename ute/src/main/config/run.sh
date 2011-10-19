ba251218df33bc7053cf38013134f4386e3bfe2b
Shaun Johnson <shaun@lmxm.net>
2011-10-19 09:03:43 -0400

    Fixed shell script used to launch UTE so that it passes arguments to UTE properly.

--- ute/src/main/config/run.sh
+++ ute/src/main/config/run.sh
@@ -1,1 +1,1 @@
-java -cp ./${project.artifactId}-${project.version}-${project.packaging}-with-dependencies.${project.packaging} ${mainPackage}.${mainClass} $1 $2 $3 $4 $5
+java -cp ./${project.artifactId}-${project.version}-${project.packaging}-with-dependencies.${project.packaging} ${mainPackage}.${mainClass} "$@"
