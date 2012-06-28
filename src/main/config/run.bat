d899e5551198b91061d86a3322b34611fc609dc0
Shaun Johnson <shaun@lmxm.net>
2012-06-28 15:13:19 -0400

    Output log messages to file instead of console.
    Updated logback to latest version. Changed logback.xml configuration file so
    that the root logger level is set to ERROR instead of INFO. Changed start.bat
    script to start the application without the associated console window.

--- src/main/config/run.bat
+++ src/main/config/run.bat
@@ -1,2 +1,2 @@
 @ECHO off
-start java -cp .\${project.artifactId}-${project.version}-${project.packaging}-with-dependencies.${project.packaging} ${mainPackage}.${mainClass} %1 %2 %3 %4 %5 %6 %7 %8 %9
+start javaw -cp .\${project.artifactId}-${project.version}-${project.packaging}-with-dependencies.${project.packaging} ${mainPackage}.${mainClass} %1 %2 %3 %4 %5 %6 %7 %8 %9
